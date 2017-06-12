2c5cb04690e80976f824be64d04a6c99e6854c13
dzhiqin <dzhiqin@126.com>
2017-06-12 19:23:59 +0800

    finish lesson2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   resources :topics
   root 'topics#index'
+  resources :topics do
+    member do
+      post 'upvote'
+      post 'decvote'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
